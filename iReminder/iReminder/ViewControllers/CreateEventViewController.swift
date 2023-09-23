d0474074d3e3ac0b4a34ddf5b4b9b69505c2cd01
Alexander Balla <110104160+ialexballa@users.noreply.github.com>
2023-09-23 20:08:23 +0300

    initial code

---
+++ iReminder/iReminder/ViewControllers/CreateEventViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CreateEventViewController.swift
+//  iReminder
+//
+//  Created by Alex Balla on 21.09.2023.
+//
+
+import UIKit
+
+class CreateEventViewController: UIViewController {
+
+}
---
+++ iReminder/iReminder/ViewControllers/EventsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  iReminder
+//
+//  Created by Alex Balla on 21.09.2023.
+//
+
+import UIKit
+
+class EventsViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    @IBOutlet weak var table: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        table.delegate = self
+        table.dataSource = self
+        
+        title = "Events"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", image: UIImage(systemName: "plus.circle.fill"), target: self, action: #selector(addEventPressed))
+    }
+    
+    @objc func addEventPressed(){
+        let createEventVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CreateEventViewController") as! CreateEventViewController
+        self.navigationController?.pushViewController(createEventVC, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 200)
+        
+    }
+}
+
